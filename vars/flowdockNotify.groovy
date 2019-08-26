82fc8701f1e3ad0fd8110b7b3415679ab619d7b4
shahanawaj <shahanawajpatel@gmail.com>
2019-08-26 13:33:42 -0400

    Update flowdockNotify.groovy

--- vars/flowdockNotify.groovy
+++ vars/flowdockNotify.groovy
@@ -9,7 +9,7 @@ def call(script, apiToken, tags = '') {
 
     tags = tags.replaceAll("\\s","")
 
-    def flowdockURL = "api.flowdock.com/flows/adpes/thunderbirds/messages/w7tVQLzRG-m_taZDcnDHUvnueX4"
+    def flowdockURL = "https://api.flowdock.com/flows/adpes/thunderbirds/messages/w7tVQLzRG-m_taZDcnDHUvnueX4"
 
     // build status of null means successful
     def buildStatus =  script.currentBuild.result ? script.currentBuild.result : 'SUCCESS'
